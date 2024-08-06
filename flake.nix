3b0b18900bc9c1c323c3fbe9e1911e1648b53373
sinh-x <sinh@sinhn.com>
2024-08-07 03:00:10 +0700

    Update to not run infinitely

--- flake.nix
+++ flake.nix
@@ -1,31 +1,34 @@
 {
-  description = "A Rust application";
+  description = "Command line tool to update public IP address to Wasabi";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        defaultPackage = pkgs.rustPlatform.buildRustPackage rec {
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacypackages.${system};
+      in {
+        defaultpackage = pkgs.rustplatform.buildrustpackage {
           pname = "ip_update";
           version = "0.1.0";
           src = ./.;
-          cargoSha256 = "sha256-QD3LgDxnAXR4VUnXwchW80vffeAp1JylGq5bKdCcdgE=";
-          buildInputs = [ pkgs.openssl ];
-          nativeBuildInputs = [ pkgs.cargo pkgs.rustc pkgs.pkg-config pkgs.openssl ];
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+          cargosha256 = "sha256-qd3lgdxnaxr4vunxwchw80vffeap1jylgq5bkdccdge=";
+          buildinputs = [pkgs.openssl];
+          nativebuildinputs = [pkgs.cargo pkgs.rustc pkgs.pkg-config pkgs.openssl];
+          ld_library_path = pkgs.lib.makelibrarypath [
             pkgs.openssl
           ];
         };
 
-        devShell = pkgs.mkShell {
-          buildInputs = [ pkgs.openssl ];
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+        devshell = pkgs.mkshell {
+          buildinputs = [pkgs.openssl];
+          ld_library_path = pkgs.lib.makelibrarypath [
             pkgs.openssl
           ];
         };
--- src/main.rs
+++ src/main.rs
@@ -12,21 +12,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .unwrap_or_else(|_| "unknown".to_string());
     let ip_file_path = format!("shared_resources/{}_public_ip.txt", hostname);
 
-    loop {
-        let current_ip = wasabi::read_from_wasabi("sinh", &ip_file_path)
-            .await
-            .unwrap_or_else(|_| String::new());
-        let new_ip = get_public_ip().await?;
+    let current_ip = wasabi::read_from_wasabi("sinh", &ip_file_path)
+        .await
+        .unwrap_or_else(|_| String::new());
+    let new_ip = get_public_ip().await?;
 
-        if new_ip != current_ip {
-            info!("Public IP: {}", new_ip);
-            wasabi::upload_to_wasabi("sinh", &ip_file_path, &new_ip).await?;
-        } else {
-            info!("Public IP has not changed");
-        }
-
-        sleep(Duration::from_secs(30 * 60)).await;
+    if new_ip != current_ip {
+        info!("Public IP: {}", new_ip);
+        wasabi::upload_to_wasabi("sinh", &ip_file_path, &new_ip).await?;
+    } else {
+        info!("Public IP has not changed");
     }
+
+    Ok(())
 }
 
 async fn get_public_ip() -> Result<String, Error> {
